6bfc289008840a9828b95d978eb241a7c59099f2
pattybobatty <patrick_ascolese@hotmail.com>
2014-01-24 22:12:15 -0800

    First Checkin BRO

---
+++ Assets/Dude.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dude : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void Kill()
+	{
+
+	}
+
+	void Stealth()
+	{
+
+	}
+
+	void Influence()
+	{
+
+	}
+}
