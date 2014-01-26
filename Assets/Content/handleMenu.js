ebf5d088da7b019811787bcf71e956209d927370
Russell Alleen-Willems <ralleenwillems@gmail.com>
2014-01-25 17:30:34 -0800

    Draft UI/Menu works
    The game now includes a menu that allows you to start the game (load
    the first level), view instructions, view credits, and exit the game.
    Check out the sweet logo Ray made too!

---
+++ Assets/Content/handleMenu.js
@@ -0,0 +1,53 @@
+﻿//handleMenu.js
+
+/*handleMenu is the logic for the project Caesar menu scene. It handles creating quick and dirty buttons to:
+a) start a new game, b) display game instructions, c) display game credits, d) exit the game*/
+
+//-----the global vars-----//
+
+var buttonWidth = 200;
+var buttonHeight= 50;
+var buttonSpacing = 10;
+
+function OnGUI(){
+//called every frame
+
+	//GUI.Button(Rect(0,0,200,100), "Hello!");
+	/*GUILayout.BeginArea(Rect(Screen.width/2 - buttonWidth/2, Screen.height/2 - spacing, buttonWidth, buttonHeight), "New Game 1");
+		if(GUILayout.Button("New Game", GUILayout.Height(50))){
+			Application.LoadLevel("FirstTestLevel");
+			}
+			GUILayout.Space(25);
+		if(GUILayout.Button("Instructions", GUILayout.Height(50))){
+			Application.LoadLevel("Instructions");
+			}
+			GUILayout.Space(25);
+		if(GUILayout.Button("Credits", GUILayout.Height(50))){
+			Application.LoadLevel("Credits");
+			}
+			GUILayout.Space(25);
+		if(GUILayout.Button("Exit", GUILayout.Height(50))){
+			Debug.Log("Clicking this button will exit the game in the compiled build");
+			Application.Quit();
+			}
+			GUILayout.Space(25);*/
+			
+	GUILayout.BeginArea(Rect(Screen.width/2 - buttonWidth/2, Screen.height/2 -200, buttonWidth, 400));
+		if(GUILayout.Button("New Game", GUILayout.Height(buttonHeight))){
+			Application.LoadLevel("FirstTestLevel");
+			}
+		GUILayout.Space(buttonSpacing);
+		if(GUILayout.Button("Instructions", GUILayout.Height(buttonHeight))){
+			Application.LoadLevel("Instructions");
+			}
+		GUILayout.Space(buttonSpacing);
+		if(GUILayout.Button("Credits", GUILayout.Height(buttonHeight))){
+			Application.LoadLevel("Credits");
+			}
+			GUILayout.Space(buttonSpacing);
+		if(GUILayout.Button("Exit", GUILayout.Height(buttonHeight))){
+			Debug.Log("Clicking this button will exit the game in the compiled build");
+			Application.Quit();
+			}
+			
+}
