96eff6edddf1cc7fce3da92edfdfc98e6524df09
Russell Alleen-Willems <ralleenwillems@gmail.com>
2014-01-26 12:47:00 -0800

    Updated text on Instructions page. Menu.unity now allows you to navigate between instructions, credits, and then back to Menu. New Game now loads levelOne.unity.

---
+++ Assets/Resources/2dArt/goMenu.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var buttonHeight = 70;
+var buttonWidth = 150;
+
+function Start () {
+	
+}
+
+function Update () {
+
+}
+
+function OnGUI (){
+	if(GUI.Button(Rect(0,0,buttonWidth,buttonHeight), "Back to Menu")){
+			Application.LoadLevel("Menu");
+			}
+}
