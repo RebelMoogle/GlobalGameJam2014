fafb1ebc07f1c8d810a3b6762566990f0a59604d
pattybobatty <patrick_ascolese@hotmail.com>
2014-01-26 00:12:45 -0800

    Add a big level and a fixed follow cam script

---
+++ Assets/FixedFollowCam.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixedFollowCam : MonoBehaviour 
+{
+	private Vector3 initalOffset;
+	// Use this for initialization
+	void Start () 
+	{
+		// Find the dude and stick to him like glue!
+		if ( Dude.player != null )
+		{
+			initalOffset =  transform.position - Dude.player.transform.position;
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ( Dude.player != null )
+		{
+			transform.position = Dude.player.transform.position + initalOffset;
+		}
+
+	}
+}
